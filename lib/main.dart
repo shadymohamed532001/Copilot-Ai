dd51809e44af9c87c61b66a2700c68497a6387cb
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-03-24 14:11:18 +0200

    Finsh ChatGpt App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:chatgpt/feature/chat/data/repo/chat_repo_impl.dart';
+import 'package:chatgpt/feature/chat/logic/cubit/chat_cubit.dart';
 import 'package:chatgpt/feature/home/presenation/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 29, 2, 74)),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => ChatCubit(ChatRepoImpl()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 29, 2, 74)),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
