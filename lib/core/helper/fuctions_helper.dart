48012b175fc0f5ad31ea3d33925913a584198d55
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-03-24 13:20:40 +0200

    Add functions_helper.dart and helper_const.dart
    Add functions_helper.dart and helper_const.dart files with initial content.

---
+++ lib/core/helper/fuctions_helper.dart
@@ -0,0 +1,19 @@
+
+
+// Future<Map<String, dynamic>> fetchDataFromLocalStorage() async {
+//   onBording = await LocalServices.getData(key: 'onbording');
+//   usertoken = await LocalServices.getData(key: 'token');
+//   log('UserToken : $usertoken');
+//   return {'onBording': onBording, 'token': usertoken};
+// }
+//  void validateEmail(String val) {
+//     if (!EmailValidator.validate(val, true) && val.isNotEmpty) {
+//       setState(() {
+//         errorMessage = "Invalid Email Address";
+//       });
+//     } else {
+//       setState(() {
+//         errorMessage = "";
+//       });
+//     }
+//   }
